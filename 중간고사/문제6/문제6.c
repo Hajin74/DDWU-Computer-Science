d6b560e2b285a043658841be688956554f6caaff
Hajin <67518596+Hajin74@users.noreply.github.com>
2021-05-15 16:43:31 +0900

    Update 문제6.c

--- 중간고사/문제6/문제6.c
+++ 중간고사/문제6/문제6.c
@@ -1,22 +1,21 @@
-// 20200988 ¿Ø«œ¡¯
+// 20200988 √Ä¬Ø√á√è√Å√∏
 #include <stdio.h>
 
-int main(void)
-{
-	int key, num;
+#include <stdio.h>
+
+int main(void) {
+	int key, num, n;
 	int i = 1;
 	int keyLocation = 0;
 
-	scanf_s("%d", &key);
-	scanf_s("%d", &num);
+	scanf("%d", &num);
+	scanf("%d", &key);
 
-	while (num != -1 && keyLocation < 1)
+	for(i = 1; i <= num; i++) 
 	{
-		keyLocation = 0;
-		i++;
-		scanf_s("%d", &num);
+		scanf("%d", &n);
 
-		if (num == key)
+		if (n == key && keyLocation == 0)
 		{
 			keyLocation = i;
 		}
@@ -26,60 +25,4 @@ int main(void)
 		printf("not found\n");
 	else
 		printf("%d\n", keyLocation);
-
-
-
-
-
-
-
-
-	/*
-	do
-	{
-		j = 0;
-
-		scanf_s("%d", &num);
-		i++;
-
-		if (num == key)
-		{
-			j = i;
-		}
-
-	} while (num != -1);
-
-	if (j == 0)
-		printf("not found\n");
-	else
-		printf("%d\n", j);
-		*/
-
 }
-
-/* ∏¬¥¬ ¡§¥‰
-int main(void)
-{
-	int key, num = 0;
-	int index = 0;
-	int found = 0;
-
-	scanf("%d", &key);
-
-	while(num != -1)
-	{
-		scanf("%d", &num);
-		index++;
-
-		if(num == key && found == 0)
-		{
-			found = index;
-		}
-	}
-
-	if(found > 0)
-		printf("%d\n", found);
-	else
-		printf("not found\n");
-}
-*/
