a9414c35687f4fa17b13f8ae9af9e3c1fe875f82
Hajin74 <yoonmij9974@naver.com>
2022-06-30 15:52:11 +0900

    week5

---
+++ week5_exam2/app/src/main/java/ddwu/mobile/week05/ex2/MainActivity.java
@@ -0,0 +1,26 @@
+package ddwu.mobile.week05.ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        MyView myView = findViewById(R.id.myView);
+        myView.setMakeCircle(1);
+        myView.invalidate();
+    }
+}
