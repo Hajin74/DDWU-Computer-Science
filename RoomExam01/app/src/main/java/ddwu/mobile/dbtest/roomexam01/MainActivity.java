5d0b4f9ab2eb288239f082350ea5facaca4e6d93
Hajin74 <yoonmij9974@naver.com>
2022-09-28 15:02:38 +0900

    Room DB 사용, thread

--- RoomExam01/app/src/main/java/ddwu/mobile/dbtest/roomexam01/MainActivity.java
+++ RoomExam01/app/src/main/java/ddwu/mobile/dbtest/roomexam01/MainActivity.java
@@ -1,14 +1,17 @@
 package ddwu.mobile.dbtest.roomexam01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class  MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,10 @@ public class  MainActivity extends AppCompatActivity {
 
 //    ArrayAdapter<Food> adapter;
 
+    // Room
+    FoodDB foodDB;
+    FoodDao foodDao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +36,11 @@ public class  MainActivity extends AppCompatActivity {
         etNation = findViewById(R.id.etNation);
         listView = findViewById(R.id.listView);
 //        adapter = new ArrayAdapter<Food>(this, android.R.layout.simple_list_item_1, new ArrayList<Food>());
-        
+
+        // Room
+        foodDB = Room.databaseBuilder(getApplicationContext(), FoodDB.class, "food_db.db").build();
+        foodDao = foodDB.foodDao();
+
     }
 
 
@@ -40,7 +51,12 @@ public class  MainActivity extends AppCompatActivity {
 
         switch (v.getId()) {
             case R.id.btnInsert:
-
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        foodDao.insertFood(new Food(food, nation));
+                    }
+                }).start();
                 break;
             case R.id.btnUpdate:
 
@@ -49,7 +65,15 @@ public class  MainActivity extends AppCompatActivity {
 
                 break;
             case R.id.btnShow:
-
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<Food> foodList = foodDao.getAllFoods();
+                        for (Food afood : foodList) {
+                            Log.d(TAG, afood.toString());
+                        }
+                    }
+                }).start();
                 break;
         }
     }
