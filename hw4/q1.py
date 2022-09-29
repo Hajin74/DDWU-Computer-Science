37820719a1ab0253a1392574c66a137c3bf6b915
Hajin74 <yoonmij9974@naver.com>
2022-09-29 18:35:23 +0900

    LABHW4 q1

---
+++ hw4/q1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+file_name = input("파일 이름을 입력하세요: ")
+input_file = open(file_name, "rt")
+output_file = input_file.read()
+
+f = open("output.txt", "wt")
+f.write(output_file.upper())
+f.close()
+	
