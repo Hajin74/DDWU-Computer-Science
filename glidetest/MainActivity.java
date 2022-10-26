ec3438745995a175ff9cd74772fe736f40da938c
Hajin <67518596+Hajin74@users.noreply.github.com>
2022-10-26 13:58:37 +0900

    Glide

---
+++ glidetest/MainActivity.java
@@ -0,0 +1,37 @@
+package ddwu.mobile.network.glidetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button:
+                String url = "https://upload.wikimedia.org/wikipedia/commons/3/31/NV_Gogol.png";
+                Glide.with(this)
+                        .load(url)
+                        .into(imageView);
+                break;
+        }
+    }
+}
